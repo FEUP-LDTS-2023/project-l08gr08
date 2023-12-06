7c0f5972d55a08763c401a2d02ed6d972e205d43
Gonca Sampa <goncalo20.sampaio03@gmail.com>
2023-12-06 15:59:22 +0000

    Made changes to Menu

--- src/main/java/bdude/model/Position.java
+++ src/main/java/bdude/model/Position.java
@@ -11,15 +11,12 @@ public class Position {
     public Position getLeft() {
         return new Position(x - 1, y);
     }
-
     public Position getRight() {
         return new Position(x + 1, y);
     }
-
     public Position getUp() {
         return new Position(x, y + 1);
     }
-
     public Position getDown() {
         return new Position(x, y - 1);
     }
