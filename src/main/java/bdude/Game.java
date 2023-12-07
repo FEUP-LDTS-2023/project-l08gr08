32ac6590fbbfa64409de8f3ff36a190e1fb9749f
Eduardo Cunha <edumrcunha@gmail.com>
2023-12-07 15:05:18 +0000

    Resources to main

--- src/main/java/bdude/Game.java
+++ src/main/java/bdude/Game.java
@@ -14,7 +14,7 @@ public class Game {
     private State state;
 
     public Game() throws FontFormatException, IOException, URISyntaxException {
-        this.gui = new LanternaGUI(200, 10);
+        this.gui = new LanternaGUI(200, 100);
         this.state = new MenuState(new Menu());
     }
 
